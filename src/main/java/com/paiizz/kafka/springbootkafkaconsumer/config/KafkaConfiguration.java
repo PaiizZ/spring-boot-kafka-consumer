09c02404735e5c05c9e28815ffd90d434765fea3
PaiizZ <wanchanapon.t@ku.th>
2018-09-27 14:15:36 +0700

    add service

--- src/main/java/com/paiizz/kafka/springbootkafkaconsumer/config/KafkaConfiguration.java
+++ src/main/java/com/paiizz/kafka/springbootkafkaconsumer/config/KafkaConfiguration.java
@@ -4,6 +4,7 @@ import org.apache.kafka.clients.consumer.ConsumerConfig;
 import org.apache.kafka.common.serialization.StringDeserializer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.annotation.EnableKafka;
 import org.springframework.kafka.config.ConcurrentKafkaListenerContainerFactory;
 import org.springframework.kafka.core.ConsumerFactory;
 import org.springframework.kafka.core.DefaultKafkaConsumerFactory;
@@ -11,6 +12,7 @@ import org.springframework.kafka.core.DefaultKafkaConsumerFactory;
 import java.util.HashMap;
 import java.util.Map;
 
+@EnableKafka
 @Configuration
 public class KafkaConfiguration {
 
@@ -26,8 +28,9 @@ public class KafkaConfiguration {
         return new DefaultKafkaConsumerFactory<>(config);
     }
 
+
     @Bean
-    public ConcurrentKafkaListenerContainerFactory<String, String> concurrentKafkaListenerContainerFactory() {
+    public ConcurrentKafkaListenerContainerFactory<String, String> kafkaListenerContainerFactory() {
         ConcurrentKafkaListenerContainerFactory<String, String> factory = new ConcurrentKafkaListenerContainerFactory();
         factory.setConsumerFactory(consumerFactory());
         return factory;
